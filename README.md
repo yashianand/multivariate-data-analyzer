387bbbeeb87a72655c60ef882459657343953fc3
Yashwanthi Anand <anandy@oregonstate.edu>
2022-03-01 13:57:56 -0800

    add skeleton

---
+++ README.md
@@ -0,0 +1,18 @@
+# CS 499/549 Visual Analytics HW 3
+
+
+You must fill out the following form. Please see Canvas for detailed instructions.
+
+---
+- Name: ____ ____
+- URL to video demo: https://..........
+- URL to deployed webpage: https://..........
+- Optional comment: __________
+
+---
+
+This repository (https://github.com/div-classroom/visualanalytics-hw3-skeleton) contains the skeleton code used in the Visual Analytics course at Oregon State University taught by Prof. Minsuk Kahng (https://minsuk.com).
+
+The digits dataset is from https://archive.ics.uci.edu/ml/datasets/Optical+Recognition+of+Handwritten+Digits through scikit-learn.
+
+
---
+++ src/App.svelte
@@ -0,0 +1,110 @@
+<!-- 
+	This is the skeleton code provided by Prof. Minsuk Kahng.
+	Please feel free to revise the existing code.
+-->
+<script>
+	import { onMount } from "svelte";
+	import { scaleLinear } from "d3-scale";
+
+	let instances;
+
+	const numClasses = 10;
+	const numBins = 10;
+	let binsByClasses = [];
+
+	onMount(async () => {
+		const fetched = await fetch("static/prediction_results.json");
+		instances = (await fetched.json()).test_instances;
+		console.log(instances);
+
+		// Transform data
+		for (let k = 0; k < numClasses; ++k) {
+			let binsForClass = [];
+			for (let b = 0; b < numBins; ++b) {
+				binsForClass.push({"class": k, "binNo": b, "instances": []});
+			}
+			binsByClasses.push({"class": k, "bins": binsForClass});
+		}
+		console.log(binsByClasses);
+
+		instances.forEach(instance => {
+			
+
+		});
+
+		
+
+	});
+
+	
+
+</script>
+
+<main>
+	<h1>Visual Analytics HW 3</h1>
+
+	<div id="container">
+		<div id="sidebar" style="width: 450px;">
+			<div id="projection-view" class="view-panel">
+				<div class="view-title">Projection View</div>
+				<svg >
+					
+
+				</svg>
+			</div>
+			<div id="selected-image-view" class="view-panel">
+				<div class="view-title">Selected Image</div>
+				<div id="selected-image-view-content">
+
+
+				</div>
+			</div>
+		</div>
+
+		<div id="main-section" style="width: 1000px;">
+			<div id="score-distributions-view" class="view-panel">
+				<div class="view-title">Score Distributions</div>
+				<svg >
+
+
+				</svg>
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	h1 {
+		font-size: 1.3rem;
+		font-weight: 500;
+		margin-top: 0;
+	}
+	#container {
+		display: flex;
+	}
+	#sidebar, #main-section {
+		display: flex;
+		flex-direction: column;
+	}
+	.view-panel {
+		border: 2px solid #eee;
+		margin-bottom: 15px;
+		margin-right: 15px;
+	}
+	.view-title {
+		background-color: #f3f3f3;
+		font-size: 1.0rem;
+		margin-bottom: 8px;
+		padding: 3px 8px 5px 12px;
+	}
+	#selected-image-view-content {
+		height: 100px;
+		padding: 15px;
+	}
+	svg {
+		margin: 5px;
+	}
+
+
+
+</style>
