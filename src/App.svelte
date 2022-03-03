c20ea7e83ffa20e8c166383abbce6d44bce42630
Delyar Tabatabai <tabatase@oregonstate.edu>
2022-03-03 01:44:44 -0800

    added inputs

--- src/App.svelte
+++ src/App.svelte
@@ -9,13 +9,14 @@
 
 	let instances;
 	let wineQualities = {};
+	let features = ["Alchohol", "Total Sulphur Dioxide", "Density", "Volatile Acidity", "PH", "Citric Acid", "Fixed Acidity", "Residual Sugar", "Chlorides", "Free Sulfur Dioxide", "Sulphates"]
 	const numClasses = 6;
 
 	function setupRadarChart(){
 		var marksCanvas = document.getElementById("marksChart");
 
 		var marksData = {
-			labels: ["Alchohol", "Total Sulphur Dioxide", "Density", "Volatile Acidity", "PH", "Citric Acid"],
+			labels: features.slice(0, 6),
 			datasets: [{
 				label: "Quality 8",
 				backgroundColor: "rgba(165,15,21,0.5)",
@@ -38,7 +39,7 @@
 				data: [0.848744594, 0.6413510818, 0.9990761098, 0.7845814179, 0.9951469788, 0.4452722985]
 			}, {
 				label: "Quality 3",
-				backgroundColor: "rgba(254,229,217,0.5)",
+				backgroundColor: "rgba(254,229,217,0.8)",
 				data: [0.8231051906, 0.4405991789, 1, 1, 1, 0.4372159091]
 			}]
 		};
@@ -70,7 +71,7 @@
 </script>
 
 <main>
-	<h1>Visual Analytics HW 3</h1>
+	<h1>Group 2</h1>
 
 	<div id="container">
 		<div id="sidebar" style="width: 450px;">
@@ -78,18 +79,24 @@
 				<div class="view-title">Radar Chart</div>
 				<canvas id="marksChart" width="600" height="400"></canvas>
 			</div>
-			<div id="selected-image-view" class="view-panel">
-				<div class="view-title">Selected Image</div>
-				<div id="selected-image-view-content">
-
-
+			<div id="input-view" class="view-panel">
+				<div class="view-title">Input View</div>
+				<div id="input-view-content">
+					<svg height="340">
+						{#each features as label,i}
+							<text x="10" y="{i*30+15}" width="80%" height="10">{label}</text>
+							<foreignObject x="170" y="{i*30}" width="170" height="20">
+								<input type="text"/>
+							</foreignObject>
+						{/each}
+					</svg>
 				</div>
 			</div>
 		</div>
 
 		<div id="main-section" style="width: 1000px;">
 			<div id="score-distributions-view" class="view-panel">
-				<div class="view-title">Score Distributions</div>
+				<div class="view-title">Parallel Coordinates</div>
 				<svg >
 
 
@@ -121,14 +128,10 @@
 		background-color: #f3f3f3;
 		font-size: 1.0rem;
 		margin-bottom: 8px;
-		padding: 3px 8px 5px 12px;
+		padding: 3px 4px 4px 4px;
 	}
-	#selected-image-view-content {
-		height: 100px;
-		padding: 15px;
-	}
-	svg {
-		margin: 5px;
+	#input-view-content {
+		height: 370px;
 	}
 
 
